0fb4991a969dea94ed5bf9dd69edcc0775645466
Jorge Alberto Garcia Delgadillo <jorgealbertogarciadelgadillo@Mac-mini-de-Jorge.local>
2025-11-15 17:58:53 -0600

    modificando runtest 10

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 echo "Iniciando ejecución de pruebas en Jenkins"
 
-# Verificar si el entorno virtual existe
-if [ ! -d "venv" ]; then
-    echo "Entorno virtual no encontrado. Creándolo..."
-    python3 -m venv venv || { echo "Error creando venv"; exit 1; }
-fi
-
-# Activar el entorno virtual
-if [ -f "venv/bin/activate" ]; then
-    source venv/bin/activate
-else
-    echo "Error: no se pudo activar el entorno virtual"
-    exit 1
-fi
-
-# Instalar dependencias
+# Instalar dependencias solo para el usuario
 echo "Instalando dependencias..."
-pip install --upgrade pip --break-system-packages
-pip install -r requirements.txt --break-system-packages
+python3 -m pip install --upgrade pip --user
+python3 -m pip install -r requirements.txt --user
 
 # Ejecutar pruebas
 echo "Ejecutando pruebas con pytest..."
-python -m pytest tests/
+python3 -m pytest tests/
 
 echo "Pruebas finalizadas. Reportes en reports"
